12cc67412ebf1a5ad1c31cd23afd82ecfe1acc36
Sarang Wadode <sarangwadode@gmail.com>
2021-06-14 11:16:19 +0530

    overview done

--- src/MyComponents/App.js
+++ src/MyComponents/App.js
@@ -1,11 +1,30 @@
+import React, { Component } from 'react'
 import './App.css';
+import Toolbar from './Toolbar'
+import Editor from './Editor'
+import Preview from './Preview'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setState = {
+      markdown: '',
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="editor">
+          <Toolbar />
+          <Editor />
+        </div>
+
+        <div className="preview">
+          <Toolbar />
+          <Preview />
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/MyComponents/Editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Editor extends Component {
+    render() {
+        return (
+            <div>
+                <textarea name="kuch bhi" id="editor" cols="30" rows="10"></textarea>
+            </div>
+        )
+    }
+}
