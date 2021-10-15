66bc2310000db151b62016283618281b3511dc4b
Sarang Wadode <sarangwadode@gmail.com>
2021-10-15 10:07:35 +0530

    markdown-previewer using marked library

--- src/MyComponents/Preview.js
+++ src/MyComponents/Preview.js
@@ -1,10 +1,26 @@
 import React from "react";
-import showdown from 'showdown'
+//import showdown from 'showdown'
+import marked from 'marked'
+import Prism from 'prismjs'
 
 const Preview = (props) => {
-    const converter = new showdown.Converter({tables: true, tasklists: true, smoothLivePreview: true, strikethrough: true});
+    // const converter = new showdown.Converter({tables: true, tasklists: true, smoothLivePreview: true, strikethrough: true});
+    // const md = props.markdown;
+    // const html = converter.makeHtml(md);
+
+    marked.setOptions({
+      breaks: true,
+      highlight: function (code) {
+        return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+      }
+    });
+    const renderer = new marked.Renderer();
+    renderer.link = function (href, title, text) {
+      return `<a target="_blank" href="${href}">${text}</a>`;
+    };
+
     const md = props.markdown;
-    const html = converter.makeHtml(md);
+    const html = marked(md, {renderer: renderer})
     return (
         <div
       dangerouslySetInnerHTML={{
